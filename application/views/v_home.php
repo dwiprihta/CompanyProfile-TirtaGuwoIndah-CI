7783d33c6a6bce77c0f972f45c7c823ba8a47353
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-08-12 20:42:21 +0700

    start create form tiket

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -62,7 +62,7 @@
           <li><a href="#portfolio">Galery</a></li>
           <li><a href="#faq">Faq</a></li>
           <li><a href="#contact"> Contact</a></li>
-          <li><a data-toggle="modal" data-target="modaltambah" class="btn btn-sm btn-warning" href="#" style="color:white;"> Pesan Tiket</a></li>
+          <li><a data-toggle="modal" data-target="#modaltambah" class="btn btn-sm btn-warning" href="#" style="color:white;"> Pesan Tiket</a></li>
 
         </ul>
       </nav><!-- .nav-menu -->
@@ -483,6 +483,32 @@
       </div>
       <?php endforeach;?>
     <!-- INI MODAL ARTIKERL -->
+
+    <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
     <!--MODAL TIKETING-->
         <div class="modal fade" id="modaltambah" tabindex="-1" role="dialog" aria-labelledby="modalTambah" aria-hidden="true">
