a1e8f9e09dedee8433d340cf7dd02e931b5a02ed
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-08-12 20:00:56 +0700

    change position of vidio

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -134,7 +134,7 @@
 
         <div class="row">
           <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch" data-aos="fade-right">
-            <a href="https://www.youtube.com/watch?v=HSCK5l6VQBo" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+            <a href="https://www.youtube.com/watch?v=HSCK5l6VQBo" class="venobox play-btn mb-3" data-vbtype="video" data-autoplay="true"></a>
           </div>
 
           <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
