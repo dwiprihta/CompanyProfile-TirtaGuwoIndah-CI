75ef3908e5ea44c3e6daa61a5de6af02c7c22bc2
dwi prihtapambudi <prihtapambudi@gmail.com>
2020-08-27 20:00:33 +0700

    fix typo

--- application/views/admin/v_tiket.php
+++ application/views/admin/v_tiket.php
@@ -303,7 +303,7 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">KONFIRMASI PEMBAYARAN TIKET</h5>
+                <h5 class="modal-title" id="exampleModalLabel">KONFIRMASI PENGGUNAAN TIKET</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
